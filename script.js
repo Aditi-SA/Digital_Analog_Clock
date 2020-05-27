38180c249170b3028276195fe03ba8f5ccbb295c
Aditi-SA <aditi.nadig10@gmail.com>
2020-05-26 22:01:15 -0400

    Added initial degrees to hour,minute and second hand via javascript

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+
+let hrPosition = 20;
+let minPosition = 130;
+let secPosition = 260;
+
+//apply as degrees in inline
+
+HOURHAND.style.transform = "rotate(" + hrPosition + "deg)"
+MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)"
+SECONDHAND.style.transform = "rotate(" + secPosition + "deg)"
