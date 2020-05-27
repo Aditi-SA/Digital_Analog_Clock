ee444e9647faa54cd10430dd934532705171903b
Aditi-SA <aditi.nadig10@gmail.com>
2020-05-26 23:15:05 -0400

    successful running clock
    Added setInterval and math logic to have a successful running clock

--- script.js
+++ script.js
@@ -16,14 +16,19 @@ let hrPosition = (hr*360/12) + (min*(360/60)/12);
 let minPosition = (min*360/60) + (sec*(360/60)/60);   //moves with sec hand
 let secPosition = sec*360/60;
 
+function  runTheClock () {
 
-//apply random degrees to clock as degrees in inline
+    hrPosition =hrPosition+(30/3600);
+    minPosition = minPosition + (6/60);
+    secPosition = secPosition + 6;
+//apply degrees to clock as degrees in inline
 
-HOURHAND.style.transform = "rotate(" + hrPosition + "deg)"
-MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)"
-SECONDHAND.style.transform = "rotate(" + secPosition + "deg)"
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)"
+    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)"
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)"
 
+}
 
+var interval = setInterval(runTheClock, 1000);
 
-
-
+//Note: Browser provides date and time details instead of Date()
