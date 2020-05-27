044a237e779871e2b1595b31cf0e5ad26195694c
Aditi-SA <aditi.nadig10@gmail.com>
2020-05-26 22:10:43 -0400

    Math logic to get current Time data displayed on screen.

--- script.js
+++ script.js
@@ -3,12 +3,27 @@ const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
 
-let hrPosition = 20;
-let minPosition = 130;
-let secPosition = 260;
+//Get current date and time
+var date = new Date();
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+console.log("Hour: " + hr + "Minute: " + min + " Seconds: " + sec);
 
-//apply as degrees in inline
+//convert time to correct degree to show on clock
+let hrPosition = (hr*360/12) + (min*(360/60)/12);
+//let minPosition = min*360/60;    //min position
+let minPosition = (min*360/60) + (sec*(360/60)/60);   //moves with sec hand
+let secPosition = sec*360/60;
+
+
+//apply random degrees to clock as degrees in inline
 
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)"
 MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)"
 SECONDHAND.style.transform = "rotate(" + secPosition + "deg)"
+
+
+
+
+
